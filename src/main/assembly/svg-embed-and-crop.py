1f0c0587ca7e81f8b96c051a31ff0a306bebc9d0
Benjamin Nanes <bnanes@emory.edu>
2013-09-01 14:37:35 -0400

    Workaround for issue finding JRE on Win x64
    - If OS is Windows, check common install directory for JRE
    - Use absolute path if present
    - Otherwise look for java in PATH

--- src/main/assembly/svg-embed-and-crop.py
+++ src/main/assembly/svg-embed-and-crop.py
@@ -1,8 +1,14 @@
-import subprocess, os.path, sys
+import subprocess, os.path, sys, platform
 cp = os.path.expandvars("${APPDATA}/inkscape/extensions/svg-embed-and-crop/*")
 if len(sys.argv) > 1:
 	f = sys.argv[1]
 else:
 	f = ""
-p = subprocess.Popen("java -classpath \"" + cp + "\" edu.emory.cellbio.svg.EmbedAndCropInkscapeEntry " + f, stdout=subprocess.PIPE)
+java = "java -cp \""
+if(platform.system() == "Windows"):
+	if os.path.exists("C:\\Program Files\\Java\\jre7\\bin\\java.exe"):
+		java = "\"C:\\Program Files\\Java\\jre7\\bin\\java.exe\" -cp \""
+	elif os.path.exists("C:\\Program Files\\Java\\jre6\\bin\\java.exe"):
+		java = "\"C:\\Program Files\\Java\\jre6\\bin\\java.exe\" -cp \""
+p = subprocess.Popen(java + cp + "\" edu.emory.cellbio.svg.EmbedAndCropInkscapeEntry " + f, stdout=subprocess.PIPE)
 print(p.communicate()[0])
