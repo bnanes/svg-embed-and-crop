a7079c5593bf61c869dd94e5d9b0323f90d8949e
Benjamin Nanes <bnanes@emory.edu>
2013-11-11 19:32:46 -0500

    Change java to javaw

--- src/main/assembly/svg-embed-and-crop.py
+++ src/main/assembly/svg-embed-and-crop.py
@@ -4,11 +4,11 @@ if len(sys.argv) > 1:
 	f = sys.argv[1]
 else:
 	f = ""
-java = "java -cp \""
+java = "javaw -cp \""
 if(platform.system() == "Windows"):
-	if os.path.exists("C:\\Program Files\\Java\\jre7\\bin\\java.exe"):
-		java = "\"C:\\Program Files\\Java\\jre7\\bin\\java.exe\" -cp \""
-	elif os.path.exists("C:\\Program Files\\Java\\jre6\\bin\\java.exe"):
-		java = "\"C:\\Program Files\\Java\\jre6\\bin\\java.exe\" -cp \""
+	if os.path.exists("C:\\Program Files\\Java\\jre7\\bin\\javaw.exe"):
+		java = "\"C:\\Program Files\\Java\\jre7\\bin\\javaw.exe\" -cp \""
+	elif os.path.exists("C:\\Program Files\\Java\\jre6\\bin\\javaw.exe"):
+		java = "\"C:\\Program Files\\Java\\jre6\\bin\\javaw.exe\" -cp \""
 p = subprocess.Popen(java + cp + "\" edu.emory.cellbio.svg.EmbedAndCropInkscapeEntry " + f, stdout=subprocess.PIPE)
 print(p.communicate()[0])
