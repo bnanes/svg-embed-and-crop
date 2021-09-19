05e241a1cb885be51301ba0dc8bbfa1926619ba4
Benjamin Nanes <bnanes@emory.edu>
2021-09-19 11:23:51 -0500

    Remove hard-coded path to javaw

--- src/main/assembly/svg-embed-and-crop.py
+++ src/main/assembly/svg-embed-and-crop.py
@@ -16,11 +16,6 @@ except ImportError:
 	else:
 		f = ""
 	java = "javaw -cp \""
-	if(platform.system() == "Windows"):
-		if os.path.exists("C:\\Program Files\\Java\\jre7\\bin\\javaw.exe"):
-			java = "\"C:\\Program Files\\Java\\jre7\\bin\\javaw.exe\" -cp \""
-		elif os.path.exists("C:\\Program Files\\Java\\jre6\\bin\\javaw.exe"):
-			java = "\"C:\\Program Files\\Java\\jre6\\bin\\javaw.exe\" -cp \""
 	p = subprocess.Popen(java + cp + "\" edu.emory.cellbio.svg.EmbedAndCropInkscapeEntry " + f, stdout=subprocess.PIPE)
 	print(p.communicate()[0])
 	exit()
